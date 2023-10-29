2127eb77240e9341e2aaa53a0d7516970c8d3312
Philip <eskov.philip@gmail.com>
2023-10-29 16:50:02 +0100

    Added part 2. Added script and plot directories.

--- plan.md
+++ plan.md
@@ -2,10 +2,10 @@
 
 ## Theory 
 
-### 1) Intoduction
+### 1) Introduction
  
 - Motivation 
-- Prerequsite knowledge
+- Prerequisite knowledge
 	- Complex numbers 
 	- Basic (MAT1100) calculus 
 	- Properties of sines and cosines
@@ -15,24 +15,24 @@
 
 ### 2) What is frequency domain? 
 
-- Intutitve explenation 
+- Intuitive explanation 
 - Give some motivational examples
 	- Sound from a piano chord over time (easy to grasp)
 	- Frequency components of EM wave (relevant to physics / chemistry(?))
 
-### 3) The Fourier transfrom 
+### 3) The Fourier transform  
 
 - Mathematical definition 
-- Focus on intution 
+- Focus on intuition 
 - Explain information of complex values 
 	- Abs = frequency response
 	- imaginary part needed for phase response 
 
 ### 4) The discrete Fourier transform (DFT) 
 
-- Why do we need to use the DFT and not the continious Fourier transform? 
+- Why do we need to use the DFT and not the continuous Fourier transform? 
 - Mathematical definition
-	- Explain changes in expression from contninous to discrete
+	- Explain changes in expression from continuous to discrete
 - Some words on the FFT 
 
 ### 5) Sampling frequency and aliasing 
@@ -57,7 +57,7 @@
 	- sp.fft.fftfreq and why the parameters are chosen in they way that they are 
 - Taking the FFT 
 	- sp.fft.fft 
-	- Normalising the absolute value
+	- Normalizing the absolute value
 
 - Plotting
 	- Limiting the x-axis (negative frequencies are weird) 
---
+++ scripts/part_2.py
@@ -0,0 +1,115 @@
+import numpy as np 
+import scipy as sp
+import matplotlib.pyplot as plt
+
+# Time domain: 
+
+start = 0
+end = 5
+N = 100000
+t = np.linspace(start, end, N)
+fs = N/(end-start)
+
+# =======Signals========:
+
+# Signal 1:
+
+A_1 = 1
+f_1 = 1 # Hz
+sig_1 = A_1*np.sin(2*np.pi*f_1*t)
+
+# Signal 2:
+
+A_2 = 3
+f_2 = 4 # Hz
+sig_2 = A_2*np.sin(2*np.pi*f_2*t)
+
+# Signal 1 + 2:
+
+sig_1a2 = sig_1 + sig_2
+
+
+# =======Plotting=======: 
+
+# Signal 1:
+
+plt.plot(t, sig_1, label='Signal 1')
+plt.grid()
+plt.title('f(t)')
+plt.xlabel('Tid (s)')
+plt.ylabel('Amplitude')
+plt.grid()
+
+plt.savefig('guide_sections/plots/p2_signal_1.png')
+plt.clf()
+
+
+# Frequency domain, signal 1:
+
+fft_1 = 2*abs(sp.fft.fft(sig_1))/N 
+fft_1_freqs = sp.fft.fftfreq(N, 1/fs)
+
+plt.plot(fft_1_freqs, fft_1, label='Signal 1, Frekvensdomene')
+plt.xlim(0, 5)
+plt.grid()
+plt.title('f(t), frekvensdomene')
+plt.xlabel('Frekvens (Hz)')
+plt.ylabel('Amplitude')
+
+plt.savefig('guide_sections/plots/p2_signal_1_FFT.png')
+plt.clf()
+
+
+# Signal 2:
+
+plt.plot(t, sig_2, label='Signal 2')
+plt.grid()
+plt.title('g(t)')
+plt.xlabel('Tid (s)')
+plt.ylabel('Amplitude')
+plt.grid()
+
+plt.savefig('guide_sections/plots/p2_signal_2.png')
+plt.clf()
+
+fft_2 = 2*abs(sp.fft.fft(sig_2))/N 
+fft_2_freqs = sp.fft.fftfreq(N, 1/fs)
+
+plt.plot(fft_2_freqs, fft_2, label='Signal 2, Frekvensdomene')
+plt.xlim(0, 5)
+plt.grid()
+plt.title('g(t), frekvensdomene')
+plt.xlabel('Frekvens (Hz)')
+plt.ylabel('Amplitude')
+
+plt.savefig('guide_sections/plots/p2_signal_2_FFT.png')
+plt.clf()
+
+
+# Signal 1 + 2:
+
+plt.plot(t, sig_1a2, label='Signal 1 and 2')
+plt.grid()
+plt.title('h(t)')
+plt.xlabel('Tid (s)')
+plt.ylabel('Amplitude')
+plt.grid()
+
+plt.savefig('guide_sections/plots/p2_signal_1a2.png')
+plt.clf()
+
+
+# Frequency domain, signal 1 + 2:
+
+fft_1a2 = 2*abs(sp.fft.fft(sig_1a2))/N 
+fft_1a2_freqs = sp.fft.fftfreq(N, 1/fs)
+
+plt.plot(fft_1a2_freqs, fft_1a2, label='Signal 1 + 2, Frekvensdomene')
+plt.xlim(0, 5)
+plt.grid()
+plt.title('h(t), frekvensdomene')
+plt.xlabel('Frekvens (Hz)')
+plt.ylabel('Amplitude')
+
+plt.savefig('guide_sections/plots/p2_signal_1a2_FFT.png')
+plt.clf()
